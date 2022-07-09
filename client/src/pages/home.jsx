15aea49f61dd5aa0a7a93fed2493100d40051b03
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-07-09 13:52:42 +0200

    adding transactions

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "@mui/material/Button";
-
+import "../App.css";
 function Home() {
   const customerRoute = () => {
     window.location.href = "/customer";
